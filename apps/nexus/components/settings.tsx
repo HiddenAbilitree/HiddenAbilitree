ca35deb677bb04828e88485e830ca7f95186dd44
Eric Zhang <me@ericzhang.dev>
2025-06-19 00:45:11 -0400

    feat: animation settings

--- apps/nexus/components/settings.tsx
+++ apps/nexus/components/settings.tsx
@@ -1,14 +1,38 @@
+'use client';
+
 import { Cog } from '@/components/icons/cog';
+import { AnimatePresence, motion } from 'motion/react';
+import { useState } from 'react';
 
-export const Settings = () => (
-  <>
-    <button className='bg-tns-black hover:bg-tns-black-hover border-tns-blue group fixed bottom-2 right-2 z-auto flex size-10 items-center justify-center rounded-xl border-2 p-2 hover:cursor-pointer'>
-      <Cog className='group-hover:text-tns-white z-50' />
-    </button>
+export const Settings = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setOpen(!open);
+        }}
+        className='bg-tns-black hover:bg-tns-black-hover border-tns-blue group fixed bottom-2 right-2 z-auto flex size-10 items-center justify-center rounded-xl border-2 p-2 hover:cursor-pointer'
+      >
+        <Cog className='group-hover:text-tns-white z-50' />
+      </button>
 
-    <div className='bg-tns-black border-tns-blue right-13 fixed bottom-3 flex h-8 w-10 flex-col justify-start gap-2 rounded-sm border-2 p-2'></div>
-  </>
-);
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ y: 100, height: 0, paddingBlock: 0 }}
+            animate={{ y: 0, height: 100, paddingBlock: '0.5rem' }}
+            exit={{ y: 100, height: 0, paddingBlock: 0 }}
+            transition={{ y: { ease: 'circOut' } }}
+            className='bg-tns-black border-tns-blue overflow-hidden right-13 fixed bottom-2 flex h-8 w-10 flex-col justify-start gap-2 rounded-sm border-2 p-2'
+          >
+            yo
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
 
 {
   /* <button className='border-tns-blue bg-tns-black hover:bg-tns-black-hover rounded-sm border-2 px-4 py-0.5'> */
