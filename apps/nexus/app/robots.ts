f1867a4c26001d0a8139ea316e120dbae1b03daf
Eric Zhang <me@ericzhang.dev>
2025-06-22 23:10:58 -0400

    feat: sitemap

--- apps/nexus/app/robots.ts
+++ apps/nexus/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    // sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: 'https://www.ericzhang.dev/sitemap.xml',
   };
 }
--- apps/nexus/app/sitemap.ts
+++ apps/nexus/app/sitemap.ts
@@ -2,5 +2,12 @@ import type { MetadataRoute } from 'next';
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [];
+  return [
+    {
+      url: 'https://www.ericzhang.dev',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
 }
