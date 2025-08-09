38ce8e3c09f4facc6f875b051b3e443e4bec58ef
Eric Zhang <me@ericzhang.dev>
2025-08-09 01:03:47 -0400

    feat: remove blog

--- apps/blog/next.config.ts
+++
@@ -1,8 +0,0 @@
-import mdx from '@next/mdx';
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-};
-
-export default mdx({})(nextConfig);
