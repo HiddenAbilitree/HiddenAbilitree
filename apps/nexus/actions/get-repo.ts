498ed137946c6271fa959bba1e60d5b231c20314
Eric Zhang <me@ericzhang.dev>
2025-09-23 20:17:49 -0400

    feat: disable get-repo cache

--- apps/nexus/actions/get-repo.ts
+++ apps/nexus/actions/get-repo.ts
@@ -2,8 +2,10 @@
 
 import { db, projects } from 'db';
 import { eq } from 'drizzle-orm';
+import { unstable_noStore } from 'next/cache';
 
 export const getRepo = async (repoId: number) => {
+  unstable_noStore();
   const res = await db
     .select({ fullName: projects.full_name, stars: projects.stargazers_count })
     .from(projects)
