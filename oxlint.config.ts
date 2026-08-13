e17984915a43c8debb9555fcd60da7e9452e757e
Eric Zhang <me@ericzhang.dev>
2026-08-12 22:19:34 -0400

    chore: fix lints

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -10,23 +10,20 @@ import {
 export default oxlintConfig([oxlintConfigBase], {
   ignorePatterns: oxlintIgnorePatterns([oxlintConfigNext]),
   overrides: [
-    oxlintOverride(['apps/nexus/**/*'], [oxlintConfigReact, oxlintConfigNext]),
-    {
-      files: ['apps/nexus/styles/fonts/index.ts'],
-      rules: {
-        'eslint/no-underscore-dangle': ['error', { allow: ['_0xProto'] }],
-      },
-    },
-    {
-      files: ['apps/nexus/components/code-block.tsx'],
-      rules: {
-        'import/no-unassigned-import': 'off',
-      },
-    },
+    oxlintOverride(
+      ['apps/nexus/**/*'],
+      [
+        oxlintConfigReact,
+        oxlintConfigNext,
+        {
+          rules: {
+            'eslint/no-underscore-dangle': ['error', { allow: ['_0xProto'] }],
+            'import/no-unassigned-import': ['error', { allow: ['server-only', '*/css'] }],
+          },
+        },
+      ],
+    ),
   ],
-  rules: {
-    'import/no-unassigned-import': ['error', { allow: ['server-only'] }],
-  },
   settings: {
     next: {
       rootDir: ['apps/nexus/'],
